5d56e4e665b1b6915623cc8c4b319a38ac0aca77
duarte-pompeu <pnt12@live.com.pt>
2015-03-04 23:12:29 +0000

    silence a warning (serialVersionUID not declared)

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exceptions/NullContentException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exceptions/NullContentException.java
@@ -1,16 +1,21 @@
 package pt.tecnico.bubbledocs.exceptions;
 
 public class NullContentException extends Exception{
+	// The serializable class NullContentException does not declare a 
+	// static final serialVersionUID field of type long
+	// from superclass Exception
+	private static final long serialVersionUID = 1L;
+	
 	private final int column, line;
+	
 	public NullContentException(int column, int line){
 		this.column = column;
 		this.line = line;
 	}
 	
 	@Override
-	public String getMessage(){
-		
-	return "NullContentException: Cell "
+	public String getMessage(){	
+		return "NullContentException: Cell "
 			+ column + ";" + line + " is empty.";
 	}
 }
