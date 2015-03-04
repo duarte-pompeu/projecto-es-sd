1e897f436eba54728954fe32179e6aac78eb1949
duarte-pompeu <pnt12@live.com.pt>
2015-03-04 16:56:32 +0000

    remove old Cell.java, replaced by the Cell generated by the dml

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/Cell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/Cell.java
@@ -1,9 +1,17 @@
 package pt.tecnico.bubbledocs.dml;
 
+import pt.tecnico.bubbledocs.content.Content;
+
 public class Cell extends Cell_Base {
     
     public Cell() {
         super();
     }
+    
+    public Content getContent(){
+		//FIXME: implement method properly
+		
+		return null;
+	}
     
 }
