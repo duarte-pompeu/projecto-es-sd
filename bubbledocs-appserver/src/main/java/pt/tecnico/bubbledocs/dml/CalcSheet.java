d6d3f8c269c306345b3b8090ca1204b8b417b4ef
Marcos Pires <marcos.danix@gmail.com>
2015-03-05 21:48:47 +0000

    Started working on CalcSheet, but I am not understanding how Fénix Framework works exactly.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/CalcSheet.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/CalcSheet.java
@@ -1,9 +1,53 @@
 package pt.tecnico.bubbledocs.dml;
 
+import pt.tecnico.bubbledocs.content.Content;
+import java.util.*;
+
 public class CalcSheet extends CalcSheet_Base {
-    
-    public CalcSheet() {
+    //this should be in .dml
+	//int id
+	//String name
+	//uncommented to remove errors
+	int lines;
+	int columns;
+	//
+	
+	//When a user requests a file by calling user.getCalcSheet(id) and only has readonly access	
+	//Dúvida, quando eu vou buscar um CalcSheet (por exemplo user.getCalcSheet(id)) é criado um objeto java novo?
+	private boolean readonly = false;
+	
+	private List<Cell> cells;
+		
+    public CalcSheet(int lines, int columns) {
         super();
+        //something should go here, not really sure. Fénix Framework is boggling my mind.
+
     }
+    
+    protected void setReadonly() {
+    	this.readonly = true;
+    }
+    
+    protected void setReadonly(boolean readonly) {
+    	this.readonly = readonly;
+    }
+    
+    protected boolean getReadonly() {
+    	return readonly;
+    }
+    
+    public Content getContent(int line, int column) {
+    	//TODO
+    	if (outsideBounds(line, column)) {
+    		throw new IllegalArgumentException("Invalid coordinate");
+    	}
+    	//return cells.get((line-1)*columns+(column-1));
+    	return null;
+    }
+    
+    private boolean outsideBounds(int line, int column) {
+    	return line < 1 || column < 1 || line > lines || column > columns;
+    }
+    
     
 }
