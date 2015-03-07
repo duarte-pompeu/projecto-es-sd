889546b27eb69b5574703173b4b73df434471025
duarte-pompeu <pnt12@live.com.pt>
2015-03-07 14:27:52 +0000

    add Range and RangeFunction stubs

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/content/Range.java
@@ -0,0 +1,11 @@
+package pt.tecnico.bubbledocs.content;
+
+import pt.tecnico.bubbledocs.dml.CalcSheet;
+
+public class Range {
+	
+	public Range (int firstLine, int firstColumn, int lastLine,
+			int lastColumn, CalcSheet sheet){
+		
+	}
+}
