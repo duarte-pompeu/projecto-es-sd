1bf70ee957a5f1e6fd3a0a3c43687caa0b8cdac4
Marcos Pires <marcos.danix@gmail.com>
2015-03-07 00:12:33 +0000

    Started to write the methods in User.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/User.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/User.java
@@ -1,13 +1,27 @@
 package pt.tecnico.bubbledocs.dml;
 import org.jdom2.Element;
 
-
+import java.util.*;
 
 public class User extends User_Base {
     
+
 	//Question. Should all _Base classes have an empty constructor?
     public User() {
     	super();
+    /*	this.ownedFilenames = new HashSet<String>(2*this.getCreatedCalcSheetSet().size());
+    	this.allFiles = new HashMap<Integer, Boolean>();
+    	for (CalcSheet sheet : this.getCreatedCalcSheetSet()) {
+    		this.ownedFilenames.add(sheet.getName());
+    	}
+    	for (CalcSheet sheet : this.getWriteableCalcSheetSet()) {
+    		allFiles.put(sheet.getId(), false);
+    	}
+    	for (CalcSheet sheet : this.getReadableCalcSheetSet()) {
+    		if (!allFiles.containsKey(sheet.getId())) {
+    			allFiles.put(sheet.getId(), true);
+    		}
+    	}*/
     }
 	
 	public User(String userName, String name, String password) {
@@ -15,7 +29,8 @@ public class User extends User_Base {
         this.setUserName(userName);
         this.setName(name);
         this.setPassword(password);
-      
+        //this.ownedFilenames = new HashSet<String>();
+        //this.allFiles = new HashMap<Integer, Boolean>();
     }
     
     /*
@@ -25,17 +40,41 @@ public class User extends User_Base {
      */
     public int createCalcSheet(String name, int lines, int columns) {
     	//TODO
-    	return 0;
+    	if (this.ownedFilenames.contains(name)) {
+    		throw new /*Existing document*/RuntimeException();
+    	}
+    	//this.ownedFilenames.add(name);
+    	CalcSheet sheet = new CalcSheet(name, lines, columns);
+    	this.addCreatedCalcSheet(sheet);
+    	this.addReadableCalcSheet(sheet);
+    	this.addWriteableCalcSheet(sheet);
+    	//this.allFiles.put(sheet.getId(), false);
+    	return sheet.getId();
     }
     
     
     /*
      * Gets the CalcSheet with given id. CalcSheet is readonly if this user
-     * only has readonly permission. This throws a CalcSheetException if the
+     * only has readonly permission. This throws a PermissionException if the
      * user can't read id.
      */
     CalcSheet getCalcSheet(int id) /*throws CalcSheetException*/ {
     	//TODO
+    	/*
+    	//Throw CalcSheetNotFoundException
+    	CalcSheet sheet = BubbleDocs.getInstance().getCalcSheet(id);
+    	
+    	if (!this.getReadableCalcSheetSet().contains(sheet) {
+    	  throw new PermissionException();
+    	}
+    	
+    	if (!this.getWriteableCalcSheetSet().contains(sheet)) {
+    	  sheet.setReadOnly();
+    	}
+    	
+    	return sheet;
+    	
+    	*/    	
     	return null;
     }
         
@@ -47,17 +86,33 @@ public class User extends User_Base {
     public void deleteCalcSheet(int id) {
     	//PRECOND: this owns id
     	//TODO
-    }
-    
-
-    
+    }    
     
     //For now I'm simplifying, this method could have splited versions
     //This returns an iterator for the files id's that the user created, the user can read 
     //and the user can read-write
     public Iterable<CalcSheet> getAllFiles() {    	
-    	//TODO
-    	return null;
+    	//OMG java lambdas!!!!!!11
+    	return () -> {
+    		return new Iterator<CalcSheet>() {
+    			Iterator<CalcSheet> readables = 
+    					User.this.getReadableCalcSheetSet().iterator();
+    			@Override
+    			public boolean hasNext() {
+    				return readables.hasNext();
+    			}
+
+    			@Override
+    			public CalcSheet next() {
+    				CalcSheet next = readables.next();
+    				if (!User.this.getWriteableCalcSheetSet().contains(next)) {
+    					next.setReadonly();
+    				}
+    				return next;
+    			}
+
+    		};
+    	};
     }
     
 
