04f1a167c43e62896e9c8cd29c4b2eeca040bc14
duarte-pompeu <pnt12@live.com.pt>
2015-03-07 17:58:56 +0000

    add class Prd

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/content/Prd.java
@@ -0,0 +1,43 @@
+package pt.tecnico.bubbledocs.content;
+
+import org.jdom2.Element;
+
+import pt.tecnico.bubbledocs.dml.Cell;
+import pt.tecnico.bubbledocs.exceptions.NullContentException;
+
+public class Prd extends RangeFunction {
+	
+	public Prd(){
+		
+	}
+	
+	public Prd(Range range){
+		super(range);
+	}
+	
+	@Override
+	public int getValue() throws NullContentException {
+		// initialize with 1 since we're multiplying
+		int total = 1;
+		
+		for(Cell cell: this.getRangeCells()){
+			total *= cell.getContent().getValue();
+		}
+		
+		return total;
+		
+	}
+
+	@Override
+	public Element exportToXML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void importFromXML(Element element) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
