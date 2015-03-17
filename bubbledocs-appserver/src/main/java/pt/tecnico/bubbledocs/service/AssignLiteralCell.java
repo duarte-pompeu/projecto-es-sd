0493a590bd0a6b17391e1887babe9ed3db92a24c
duarte-pompeu <pnt12@live.com.pt>
2015-03-17 19:55:19 +0000

    worked on #10 some more

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignLiteralCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignLiteralCell.java
@@ -8,14 +8,28 @@ import pt.tecnico.bubbledocs.dml.Literal;
 import pt.tecnico.bubbledocs.dml.User;
 import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
 import pt.tecnico.bubbledocs.exceptions.NotFoundException;
+import pt.tecnico.bubbledocs.exceptions.PermissionException;
 
 public class AssignLiteralCell extends BubbleDocsService{
     private String result;
+    private String accessUsername;
+    int docId;
+    String cellId;
+    String literal;
 
     // TODO:AssignLiteralCell: finish service
     // TODO:AssignLiteralCell: test
-    public AssignLiteralCell(String accessUsername, int docId, String cellId, String literal) 
-    		throws NotFoundException{
+    public AssignLiteralCell(String accessUsername, int docId, String cellId, String literal) {
+    	
+    	this.accessUsername = accessUsername;
+    	this.docId = docId;
+    	this.cellId = cellId;
+    	this.literal = literal;
+    }
+
+    @Override
+    protected void dispatch() throws BubbleDocsException {
+    	//TODO: make sure it's a literal
     	
     	BubbleDocs bd = BubbleDocs.getInstance();
     	CalcSheet cs = null;
@@ -38,12 +52,10 @@ public class AssignLiteralCell extends BubbleDocsService{
     	// FIXME: AssignLiteralCell: this is a weird way to get a cell line and column
     	// we find the cell, get the object, and then access it again
     	Cell cell = cs.getCell(Integer.valueOf(cellId));
-    	cs.setContent(user, content, cell.getLine(), cell.getColumn());
-    }
-
-    @Override
-    protected void dispatch() throws BubbleDocsException {
-	// add code here
+    	try{
+    		cs.setContent(user, content, cell.getLine(), cell.getColumn());
+    	}
+    	catch (PermissionException permException){	throw permException;	}
     }
 
     public String getResult() {
