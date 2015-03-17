ecec878e4aab01a5d0928a5a112787db8f7db76d
Marcos Pires <marcos.danix@gmail.com>
2015-03-17 12:30:33 +0000

    started writing CreateUser service.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -1,17 +1,33 @@
 package pt.tecnico.bubbledocs.service;
 
+import pt.tecnico.bubbledocs.dml.BubbleDocs;
+import pt.tecnico.bubbledocs.dml.User;
+
+
 // add needed import declarations
 import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
 
 public class CreateUser extends BubbleDocsService {
 
+	private String token;
+	private String username;
+	private String name;
+	private String password;
+	
     public CreateUser(String userToken, String newUsername,
             String password, String name) {
-	// add code here
+    	this.token = userToken;
+    	this.username = newUsername;
+    	this.name = name;
+    	this.password = password;
     }
-
+    
     @Override
     protected void dispatch() throws BubbleDocsException {
-	// add code here
+    	//Código para autenticação, temos de decidir o método.
+    	//User user = Session.getInstance().getUser(token);
+    	//TODO
+    	User user = null; //Given the token, get the user.
+    	user.createUser(username, name, password);    	
     }
 }
