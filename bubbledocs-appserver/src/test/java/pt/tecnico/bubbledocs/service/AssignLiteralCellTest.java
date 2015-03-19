3b1de756b41ab0afa31f3cb5dfee16b40e1ee780
duarte-pompeu <pnt12@live.com.pt>
2015-03-19 19:14:44 +0000

    put files in correct folder fixes #14

---
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/AssignLiteralCellTest.java
@@ -0,0 +1,56 @@
+package pt.tecnico.bubbledocs.service;
+
+import org.junit.Test;
+
+import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
+
+public class AssignLiteralCellTest extends BubbleDocsServiceTest {
+	/* FIXME: o Eclipse não está a reconhecer bem as classes no meu PC
+	 * vou submeter coisas comentadas para tentar adiantar trabalho
+	 * enquanto não resolvo o problema
+	 * 
+	 * Para fazer toggle comentários rapidamente: Ctrl+A -> Ctrl+Shit+C
+	 * -Pompeu
+	 */
+    
+	String userName;
+	int docId;
+	
+	@Override
+	// TODO: login user before inserting
+	public void populate4Test(){
+		// TODO:
+		// populate userName
+		// populate docId
+		
+	}
+	
+	@Test(expected = BubbleDocsException.class)
+	public void cellDoesntExist(){
+		String int_as_string = "0";
+		
+		// TODO:
+		//make sure this is an impossible cellID
+		int bad_cell_id = -123;
+		AssignLiteralCell service = new AssignLiteralCell(userName, docId, bad_cell_id, int_as_string);
+		service.dispatch();
+	}
+	
+	//TODO: ALCTest complete stub method
+	@Test
+	public void docDoesntExist(){
+		
+	}
+	
+	@Test
+	//TODO: ALCTest complete stub method
+	public void cellIsProtected(){
+		
+	}
+	
+	@Test
+	//TODO: ALCTest complete stub method
+	public void noWriteAccess(){
+		
+	}
+}
