ddb0b63b168317cfa48485408b56d67b917b7b76
duarte-pompeu <pnt12@live.com.pt>
2015-03-19 19:56:49 +0000

    populate and simpleSuccess #10

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/AssignLiteralCellTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/AssignLiteralCellTest.java
@@ -1,28 +1,51 @@
 package pt.tecnico.bubbledocs.service;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import pt.tecnico.bubbledocs.dml.CalcSheet;
+import pt.tecnico.bubbledocs.dml.User;
 import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
 
 public class AssignLiteralCellTest extends BubbleDocsServiceTest {
-	/* FIXME: o Eclipse não está a reconhecer bem as classes no meu PC
-	 * vou submeter coisas comentadas para tentar adiantar trabalho
-	 * enquanto não resolvo o problema
-	 * 
-	 * Para fazer toggle comentários rapidamente: Ctrl+A -> Ctrl+Shit+C
-	 * -Pompeu
-	 */
-    
 	String userName;
 	int docId;
 	
+	private final String U_NAME = "jubileu";
+	private final String U_PASS = "password";
+	
+	private User USER;
+	
+	private CalcSheet SH_SHEET;
+	private int SH_ID;
+	private final String SH_NAME = "Cabulas ES";
+	private final int SH_ROWS = 10;
+	private final int SH_LINES = 10;
+	
+	private int CELL_ID0;
+	private final int VAL0 = 0;
+	private final String LIT0 = "0";
+
+	
 	@Override
 	// TODO: login user before inserting
+	// TODO: create a calcsheet
 	public void populate4Test(){
-		// TODO:
-		// populate userName
-		// populate docId
+		USER = createUser(U_NAME, U_PASS, "Jubileu Mandafacas");
+		addUserToSession(U_NAME);
 		
+		SH_SHEET = createSpreadSheet(USER, SH_NAME, SH_ROWS, SH_LINES);
+		SH_ID = SH_SHEET.getId();
+		CELL_ID0 = SH_SHEET.getCell(1, 1).getId();
+		
+	}
+	
+	@Test
+	public void simpleSuccess(){
+		AssignLiteralCell service = new AssignLiteralCell(U_NAME, SH_ID, CELL_ID0, LIT0);
+		service.dispatch();
+		assertEquals(VAL0, SH_SHEET.getCell(CELL_ID0).getContent().getValue());
 	}
 	
 	@Test(expected = BubbleDocsException.class)
@@ -32,23 +55,24 @@ public class AssignLiteralCellTest extends BubbleDocsServiceTest {
 		// TODO:
 		//make sure this is an impossible cellID
 		int bad_cell_id = -123;
+		
 		AssignLiteralCell service = new AssignLiteralCell(userName, docId, bad_cell_id, int_as_string);
 		service.dispatch();
 	}
 	
 	//TODO: ALCTest complete stub method
-	@Test
+	@Test(expected = BubbleDocsException.class)
 	public void docDoesntExist(){
 		
 	}
 	
-	@Test
+	@Test(expected = BubbleDocsException.class)
 	//TODO: ALCTest complete stub method
 	public void cellIsProtected(){
 		
 	}
 	
-	@Test
+	@Test(expected = BubbleDocsException.class)
 	//TODO: ALCTest complete stub method
 	public void noWriteAccess(){
 		
