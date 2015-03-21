duarte-pompeu/projecto-es-sd

commit b3540035df99034298a6615553291da9f90da440

fixed expired Session bug.
That statement works backwards after all

bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/Session.java
@@ -16,7 +16,7 @@ public class Session extends Session_Base {
     }
     
     public boolean isExpired() {
-    	return this.getExpiration().isBefore(new LocalTime());
+    	return this.getExpiration().isAfter(new LocalTime());
     }
     
     //time is the instant being tested

commit 0738170a1d854c4ba97327016835bf6990dac6c4

Fixed the bug i introduced in b354003

bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/Session.java
@@ -16,7 +16,7 @@ public class Session extends Session_Base {
     }
     
     public boolean isExpired() {
-    	return this.getExpiration().isAfter(new LocalTime());
+    	return this.getExpiration().isBefore(new LocalTime());
     }
     
     //time is the instant being tested