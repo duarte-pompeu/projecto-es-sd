aeb88a12fae0dd9be4fd9669bfe32e95546566ec
Marcos Pires <marcos.danix@gmail.com>
2015-03-21 11:39:41 +0000

    Added ServiceTestSuite.
    This already tests LoginUserTest and AssignLiteralCellTest. You can add
    more tests. This allows to do one quick test of the implemented tests
    before pushing. (Useful for repo-wreckers like me).

---
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/ServiceTestSuite.java
@@ -0,0 +1,20 @@
+package pt.tecnico.bubbledocs.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+//To test this you can run:
+//    mvn test -Dtest=*Suite
+//
+//To run one individual test you can run
+//    mvn test -Dtest=Login*
+//or  mvn test -Dtest=*Literal*
+//and of course you can use eclipse.
+//Add more tests when they are done.
+
+@RunWith(Suite.class)
+@SuiteClasses({ AssignLiteralCellTest.class, LoginUserTest.class })
+public class ServiceTestSuite {
+
+}
