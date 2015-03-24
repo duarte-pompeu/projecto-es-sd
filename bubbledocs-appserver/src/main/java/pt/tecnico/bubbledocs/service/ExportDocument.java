ab18a70b792ec8f91cd200543bb074f89e5c3513
Tiago <ti_tyago@hotmail.com>
2015-03-24 12:27:09 +0000

    [export]service and test

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportDocument.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportDocument.java
@@ -1,21 +1,51 @@
 package pt.tecnico.bubbledocs.service;
 
+import pt.tecnico.bubbledocs.dml.BubbleDocs;
+import pt.tecnico.bubbledocs.BubbleApplication;
+import pt.tecnico.bubbledocs.dml.CalcSheet;
+import pt.tecnico.bubbledocs.dml.User;
 // add needed import declarations
 import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
+import pt.tecnico.bubbledocs.exceptions.NotFoundException;
 
 public class ExportDocument extends BubbleDocsService {
     private byte[] docXML;
-
+    String userToken;
+    int docId;
+    
+    
     public byte[] getDocXML() {
 	return docXML;
     }
 
     public ExportDocument(String userToken, int docId) {
-	// add code here
+    	this.userToken=userToken;
+    	this.docId=docId;
     }
 
     @Override
     protected void dispatch() throws BubbleDocsException {
-	// add code here
+    	org.jdom2.Document d=null;
+		BubbleDocs pb = BubbleDocs.getInstance();
+		User user;
+		CalcSheet c = null;
+    	try{
+    		user = getSessionFromToken(userToken).getUser();
+    	}catch(BubbleDocsException e){
+    		System.out.println(e.toString()+e.getMessage());
+    	}
+    	
+    	for(CalcSheet s : pb.getCalcSheetSet()){
+    		if(s.getId()==docId){
+    			c=s;
+    			break;
+    		}
+    	}
+    	
+    	if(c==null)
+    		throw new NotFoundException();
+    	d=BubbleApplication.convertToXML(c);
+    	
+    	
     }
 }
