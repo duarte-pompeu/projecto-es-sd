dc0ab35928f5719463d563586e3cd3519cbe9755
Marcos Pires <marcos.danix@gmail.com>
2015-03-24 16:31:57 +0000

    [SD-ID] Finished SD-ID skeletal.

--- sd-id/src/main/java/pt/tecnico/sd/id/SDIdImpl.java
+++ sd-id/src/main/java/pt/tecnico/sd/id/SDIdImpl.java
@@ -1,9 +1,16 @@
-package pt.tecnicno.sd-id;
+package pt.tecnico.sd.id;
 
 //This class implements the service
 
 import javax.jws.*;
 
+import pt.ulisboa.tecnico.sdis.id.ws.AuthReqFailed_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.EmailAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidEmail_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidUser_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.SDId;
+import pt.ulisboa.tecnico.sdis.id.ws.UserAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserDoesNotExist_Exception;
 
 @WebService(
     endpointInterface="pt.ulisboa.tecnico.sdis.id.ws.SDId", 
@@ -14,6 +21,33 @@ import javax.jws.*;
     serviceName="SDId"
 )
 public class SDIdImpl implements SDId {
+
+	@Override
+	public void createUser(String userId, String emailAddress)
+			throws EmailAlreadyExists_Exception, InvalidEmail_Exception,
+			InvalidUser_Exception, UserAlreadyExists_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void renewPassword(String userId) throws UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeUser(String userId) throws UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public byte[] requestAuthentication(String userId, byte[] reserved)
+			throws AuthReqFailed_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 }
