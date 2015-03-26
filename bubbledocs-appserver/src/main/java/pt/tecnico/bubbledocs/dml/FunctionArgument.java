2c9f19db6ee4ec96654e2786e567ada445439ccf
Marcos Pires <marcos.danix@gmail.com>
2015-03-26 16:44:05 +0000

    remove unecessary @Override

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/FunctionArgument.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/FunctionArgument.java
@@ -21,19 +21,19 @@ public abstract class FunctionArgument extends FunctionArgument_Base {
 	/* (non-Javadoc)
 	 * @see pt.tecnico.bubbledocs.dml.Content#exportToXML()
 	 */
-	@Override
+	
 	public abstract Element exportToXML();
 
 	/* (non-Javadoc)
 	 * @see pt.tecnico.bubbledocs.dml.Content#getValue()
 	 */
-	@Override
+
 	public abstract int getValue() throws NullContentException;
 
 	/* (non-Javadoc)
 	 * @see pt.tecnico.bubbledocs.dml.Content#importFromXML(org.jdom2.Element)
 	 */
-	@Override
+
 	public abstract void importFromXML(Element c);
     
 }
