e97c6385ec23de3e81dd24e3671a6111acc6933c
Marcos Pires <marcos.danix@gmail.com>
2015-03-26 16:43:22 +0000

    Added classes generated from dml.
    -LiteralArgument
    -ReferenceArgument

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/dml/LiteralArgument.java
@@ -0,0 +1,31 @@
+package pt.tecnico.bubbledocs.dml;
+
+import org.jdom2.Element;
+
+import pt.tecnico.bubbledocs.exceptions.NullContentException;
+
+public class LiteralArgument extends LiteralArgument_Base {
+    
+    public LiteralArgument() {
+        super();
+    }
+
+	@Override
+	public Element exportToXML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getValue() throws NullContentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void importFromXML(Element c) {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}
