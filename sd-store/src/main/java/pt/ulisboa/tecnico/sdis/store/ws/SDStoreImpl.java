d9aa3310809836311da2dca235fe284fa3b6b2a6
duarte-pompeu <pnt12@live.com.pt>
2015-03-30 11:22:36 +0100

    simplify arguments thanks Eclipse

--- sd-store/src/main/java/pt/ulisboa/tecnico/sdis/store/ws/SDStoreImpl.java
+++ sd-store/src/main/java/pt/ulisboa/tecnico/sdis/store/ws/SDStoreImpl.java
@@ -1,13 +1,8 @@
 package pt.ulisboa.tecnico.sdis.store.ws;
 
 import java.util.List;
-import javax.jws.WebMethod;
-import javax.jws.WebParam;
-import javax.jws.WebResult;
+
 import javax.jws.WebService;
-import javax.xml.bind.annotation.XmlSeeAlso;
-import javax.xml.ws.RequestWrapper;
-import javax.xml.ws.ResponseWrapper;
 
 @WebService(
 	endpointInterface="pt.ulisboa.tecnico.sdis.store.ws.SDStore",
@@ -19,77 +14,33 @@ import javax.xml.ws.ResponseWrapper;
 )
 
 public class SDStoreImpl implements SDStore{
-	
-    /**
-     * 
-     * @param docUserPair
-     * @throws DocAlreadyExists_Exception
-     */
-    @WebMethod
-    @RequestWrapper(localName = "createDoc", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.CreateDoc")
-    @ResponseWrapper(localName = "createDocResponse", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.CreateDocResponse")
-    public void createDoc(
-        @WebParam(name = "docUserPair", targetNamespace = "")
-        DocUserPair docUserPair)
-        throws DocAlreadyExists_Exception
-    {}
 
-    /**
-     * 
-     * @param userId
-     * @return
-     *     returns java.util.List<java.lang.String>
-     * @throws UserDoesNotExist_Exception
-     */
-    @WebMethod
-    @WebResult(name = "documentId", targetNamespace = "")
-    @RequestWrapper(localName = "listDocs", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.ListDocs")
-    @ResponseWrapper(localName = "listDocsResponse", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.ListDocsResponse")
-    public List<String> listDocs(
-        @WebParam(name = "userId", targetNamespace = "")
-        String userId)
-        throws UserDoesNotExist_Exception
-    {
+	@Override
+	public void createDoc(DocUserPair docUserPair)
+			throws DocAlreadyExists_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> listDocs(String userId)
+			throws UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-    /**
-     * 
-     * @param contents
-     * @param docUserPair
-     * @throws UserDoesNotExist_Exception
-     * @throws DocDoesNotExist_Exception
-     * @throws CapacityExceeded_Exception
-     */
-    @WebMethod
-    @RequestWrapper(localName = "store", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.Store")
-    @ResponseWrapper(localName = "storeResponse", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.StoreResponse")
-    public void store(
-        @WebParam(name = "docUserPair", targetNamespace = "")
-        DocUserPair docUserPair,
-        @WebParam(name = "contents", targetNamespace = "")
-        byte[] contents)
-        throws CapacityExceeded_Exception, DocDoesNotExist_Exception, UserDoesNotExist_Exception
-    {
+	@Override
+	public void store(DocUserPair docUserPair, byte[] contents)
+			throws CapacityExceeded_Exception, DocDoesNotExist_Exception,
+			UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
-    /**
-     * 
-     * @param docUserPair
-     * @return
-     *     returns byte[]
-     * @throws UserDoesNotExist_Exception
-     * @throws DocDoesNotExist_Exception
-     */
-    @WebMethod
-    @WebResult(name = "contents", targetNamespace = "")
-    @RequestWrapper(localName = "load", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.Load")
-    @ResponseWrapper(localName = "loadResponse", targetNamespace = "urn:pt:ulisboa:tecnico:sdis:store:ws", className = "pt.ulisboa.tecnico.sdis.store.ws.LoadResponse")
-    public byte[] load(
-        @WebParam(name = "docUserPair", targetNamespace = "")
-        DocUserPair docUserPair)
-        throws DocDoesNotExist_Exception, UserDoesNotExist_Exception
-    {
+	@Override
+	public byte[] load(DocUserPair docUserPair)
+			throws DocDoesNotExist_Exception, UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
