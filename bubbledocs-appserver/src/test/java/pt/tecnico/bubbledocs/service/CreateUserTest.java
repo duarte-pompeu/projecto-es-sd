3695e9f9be7e94aefc0d76bbcc00b29a2fae977f
jamartins <martins.joao.a@gmail.com>
2015-03-30 04:42:35 +0100

    Update CreateUserTest.java

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/CreateUserTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/CreateUserTest.java
@@ -1,71 +1,77 @@
-//package pt.tecnico.bubbledocs.service;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import org.junit.Test;
-//
-//import pt.tecnico.bubbledocs.dml.User;
-//
-//// add needed import declarations
-//
-//public class CreateUserTest extends BubbleDocsServiceTest {
-//
-//    // the tokens
-//    private String root;
-//    private String ars;
-//
-//    private static final String USERNAME = "ars";
-//    private static final String PASSWORD = "ars";
-//    private static final String ROOT_USERNAME = "root";
-//    private static final String USERNAME_DOES_NOT_EXIST = "no-one";
-//
-//    @Override
-//    public void populate4Test() {
-//        createUserUSERNAME, PASSWORD, "António Rito Silva");
-//        root = addUserToSession("root");
-//        ars = addUserToSession("ars");
-//    }
-//
-//    @Test
-//    public void success() {
-//        CreateUser service = new CreateUser(root, USERNAME_DOES_NOT_EXIST, "jose",
-//                "José Ferreira");
-//        service.execute();
-//
-//	// User is the domain class that represents a User
-//        User user = getUserFromUsername(USERNAME_DOES_NOT_EXIST);
-//
-//        assertEquals(USERNAME_DOES_NOT_EXIST, user.getUsername());
-//        assertEquals("jose", user.getPassword());
-//        assertEquals("José Ferreira", user.getName());
-//    }
-//
-//    @Test(expected = DuplicateUsernameException.class)
-//    public void usernameExists() {
-//        CreateUser service = new CreateUser(root, USERNAME, "jose",
-//                "José Ferreira");
-//        service.execute();
-//    }
-//
-//    @Test(expected = EmptyUsernameException.class)
-//    public void emptyUsername() {
-//        CreateUser service = new CreateUser(root, "", "jose", "José Ferreira");
-//        service.execute();
-//    }
-//
-//    @Test(expected = UnauthorizedOperationException.class)
-//    public void unauthorizedUserCreation() {
-//        CreateUser service = new CreateUser(ars, USERNAME_DOES_NOT_EXIST, "jose",
-//                "José Ferreira");
-//        service.execute();
-//    }
-//
-//    @Test(expected = UserNotInSessionException.class)
-//    public void accessUsernameNotExist() {
-//        removeUserFromSession(root);
-//        CreateUser service = new CreateUser(root, USERNAME_DOES_NOT_EXIST, "jose",
-//                "José Ferreira");
-//        service.execute();
-//    }
-//
-//}
+package pt.tecnico.bubbledocs.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import pt.tecnico.bubbledocs.domain.CalcSheet;
+import pt.tecnico.bubbledocs.domain.Reference;
+import pt.tecnico.bubbledocs.domain.User;
+import pt.tecnico.bubbledocs.exceptions.InvalidValueException;
+import pt.tecnico.bubbledocs.exceptions.RepeatExceptionException;
+import pt.tecnico.bubbledocs.exceptions.PermissionException;
+import pt.tecnico.bubbledocs.exceptions.UserNotInSessionException;
+
+
+public class CreateUserTest extends BubbleDocsServiceTest {
+
+    // the tokens
+    private String root;
+    private String user_token;
+
+	private User USER;
+    private static final String USERNAME = "tartaruga";
+    private static final String PASSWORD = "pizza";
+	private static final String NAME = "Franklin" ; 
+    private static final String ROOT_USERNAME = "root";
+    private static final String ALT_USERNAME = "rato";
+
+    @Override
+    public void populate4Test() {
+		root = addUserToSession("root");
+        USER = createUser(USERNAME, PASSWORD, NAME);
+        user_token = addUserToSession(USERNAME);
+    }
+
+    @Test
+    public void success() {
+        CreateUser service = new CreateUser(root, ALT_USERNAME, "queijo",
+                "Sensei");
+        service.execute();
+
+	// User is the domain class that represents a User
+        User user = getUserFromUsername(ALT_USERNAME);
+
+        assertEquals(ALT_USERNAME, user.getUsername());
+        assertEquals("queijo", user.getPassword());
+        assertEquals("Sensei", user.getName());
+    }
+
+    @Test(expected =  RepeatedIdentificationException.class)
+    public void usernameExists() {
+        CreateUser service = new CreateUser(root, USERNAME, PASSWORD, NAME);
+        service.execute();
+    }
+
+    @Test(expected = InvalidValueException.class)
+    public void emptyUsername() {
+        CreateUser service = new CreateUser(root, "", "queijo", "sensei");
+        service.execute();
+    }
+
+    @Test(expected = PermissionException.class)
+    public void unauthorizedUserCreation() {
+        CreateUser service = new CreateUser(user_token, ALT_USERNAME, "queijo",
+                "Sensei");
+        service.execute();
+    }
+
+    @Test(expected = UserNotInSessionException.class)
+    public void accessUsernameNotExist() {
+        removeUserFromSession(root);
+        CreateUser service = new CreateUser(root, ALT_USERNAME, "queijo",
+                "Sensei");
+        service.execute();
+    }
+
+}
