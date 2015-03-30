9540ad37b9af99c9e3f4c0082649d9c85aed91d0
Marcos Pires <marcos.danix@gmail.com>
2015-03-30 21:28:25 +0100

    Oops, fixed identation.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignReferenceCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignReferenceCell.java
@@ -11,47 +11,47 @@ import pt.tecnico.bubbledocs.exceptions.NotFoundException; //incorrect Cell or R
 import pt.tecnico.bubbledocs.exceptions.PermissionException; //User doesnt have write permissions
 
 public class AssignReferenceCell extends BubbleDocsService {
-    private String result;
-    private String accessToken;
-    private int docId;
-    private String cellId;
-    private String refId;
+	private String result;
+	private String accessToken;
+	private int docId;
+	private String cellId;
+	private String refId;
 
-    public AssignReferenceCell(String tokenUser, int docId, String cellId, String reference) {
+	public AssignReferenceCell(String tokenUser, int docId, String cellId, String reference) {
 		this.accessToken = tokenUser;
 		this.docId = docId;
 		this.cellId = cellId;
 		this.refId = reference;
-			
-    }
 
-    @Override
-    protected void dispatch() throws InvalidFormatException, NotFoundException, PermissionException {
+	}
+
+	@Override
+	protected void dispatch() throws InvalidFormatException, NotFoundException, PermissionException {
 		//token in session
-    	BubbleDocs bd = BubbleDocs.getInstance();
-    	User user;
-    	try{
-    		user = getSessionFromToken(accessToken).getUser();
-    	}
-    	catch(BubbleDocsException e){
-    		throw e;
-    	}
-		
-		CalcSheet c1 = bd.getCalcSheetById(docId);
-    	
-		Cell cell = c1.getCell(cellId);
-    	if (cell == null) throw new NotFoundException("Cell out of bounds in " + cellId);
-    	
-    	Cell refcell = c1.getCell(refId);
-		if (refcell == null) throw new NotFoundException("Reference out of bounds in " + refId);
-    	
-		c1.setContent(user, new Reference(c1.getCell(refId)), cellId);
-		
-		result = Integer.toString(cell.getContent().getValue());
-		
-    }
+		BubbleDocs bd = BubbleDocs.getInstance();
+		User user;
+		try{
+			user = getSessionFromToken(accessToken).getUser();
+		}
+		catch(BubbleDocsException e){
+			throw e;
+		}
 
-    public final String getResult() {
-        return result;
-    }
+		CalcSheet c1 = bd.getCalcSheetById(docId);
+
+		Cell cell = c1.getCell(cellId);
+		if (cell == null) throw new NotFoundException("Cell out of bounds in " + cellId);
+
+		Cell refcell = c1.getCell(refId);
+		if (refcell == null) throw new NotFoundException("Reference out of bounds in " + refId);
+
+		c1.setContent(user, new Reference(c1.getCell(refId)), cellId);
+
+		result = Integer.toString(cell.getContent().getValue());
+
+	}
+
+	public final String getResult() {
+		return result;
+	}
 }
