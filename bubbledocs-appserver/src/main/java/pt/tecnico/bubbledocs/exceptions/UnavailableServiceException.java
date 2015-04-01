39faa46bb87751168f3f852f42f5e32229e6abd0
Marcos Pires <marcos.danix@gmail.com>
2015-04-01 10:26:36 +0100

    Create UnavailableServiceException.java

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exceptions/UnavailableServiceException.java
@@ -0,0 +1,7 @@
+package pt.tecnico.bubbledocs.exceptions;
+
+public class UnavailableServiceException extends BubbleDocsException {
+
+	private static final long serialVersionUID = 198952681247438212L;
+
+}
