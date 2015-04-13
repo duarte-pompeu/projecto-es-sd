a1816b07947fe80b893aadc9448a8fc5114fdce9
Marcos Pires <marcos.danix@gmail.com>
2015-04-13 11:30:40 +0100

    [SD-ID] Added SDIdMain - the server

---
+++ sd-id/src/main/java/pt/tecnico/sd/id/SDIdMain.java
@@ -0,0 +1,69 @@
+package example.ws.impl;
+
+import javax.xml.ws.Endpoint;
+
+import example.ws.uddi.UDDINaming;
+
+//This class implements the server
+
+public class SDIdMain {
+
+    public static void main(String[] args) {
+        // Check arguments
+        if (args.length < 3) {
+            System.err.println("Argument(s) missing!");
+            System.err.printf("Usage: java %s uddiURL wsName wsURL%n", SDIdMain.class.getName());
+            return;
+        }
+
+        String uddiURL = args[0];
+        String name = args[1];
+        String url = args[2];
+
+        Endpoint endpoint = null;
+        UDDINaming uddiNaming = null;
+        try {
+            endpoint = Endpoint.create(new SDIdImpl());
+
+            // publish endpoint
+            System.out.printf("Starting %s%n", url);
+            endpoint.publish(url);
+
+            // publish to UDDI
+            System.out.printf("Publishing '%s' to UDDI at %s%n", name, uddiURL);
+            uddiNaming = new UDDINaming(uddiURL);
+            uddiNaming.rebind(name, url);
+
+            // wait
+            System.out.println("Awaiting connections");
+            System.out.println("Press enter to shutdown");
+            System.in.read();
+
+        } catch(Exception e) {
+            System.out.printf("Caught exception: %s%n", e);
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (endpoint != null) {
+                    // stop endpoint
+                    endpoint.stop();
+                    System.out.printf("Stopped %s%n", url);
+                }
+            } catch(Exception e) {
+                System.out.printf("Caught exception when stopping: %s%n", e);
+            }
+            try {
+                if (uddiNaming != null) {
+                    // delete from UDDI
+                    uddiNaming.unbind(name);
+                    System.out.printf("Deleted '%s' from UDDI%n", name);
+                }
+            } catch(Exception e) {
+                System.out.printf("Caught exception when deleting: %s%n", e);
+            }
+        }
+
+    }
+
+}
