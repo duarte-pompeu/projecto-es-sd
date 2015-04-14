31de151bb15ab6e880e3639166725142bfd4b089
Filipe <fmorgad093@gmail.com>
2015-04-15 00:13:23 +0100

    service createuser

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -5,29 +5,47 @@ package pt.tecnico.bubbledocs.service;
 import pt.tecnico.bubbledocs.domain.*;
 // add needed import declarations
 import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
-import pt.tecnico.bubbledocs.exceptions.InvalidValueException; //Case empty username
-import pt.tecnico.bubbledocs.exceptions.RepeatedIdentificationException;
-import pt.tecnico.bubbledocs.exceptions.UserNotInSessionException; //incorrect Cell or Reference given
+import pt.tecnico.bubbledocs.exceptions.RemoteInvocationException;
 import pt.tecnico.bubbledocs.exceptions.PermissionException; //User doesnt have create permissions
+import pt.tecnico.bubbledocs.exceptions.UnavailableServiceException;
+import pt.tecnico.bubbledocs.service.remote.IDRemoteServices;
 
 public class CreateUser extends BubbleDocsService {
 
 	private String token;
 	private String username;
+	private String email;
 	private String name;
 	private String password;
 	
     public CreateUser(String userToken, String newUsername,
-            String password, String name) {
+            String email, String name) {
     	this.token = userToken;
     	this.username = newUsername;
     	this.name = name;
-    	this.password = password;
+    	this.email = email;
+    	this.password = null;
     }
     
     @Override
-    protected void dispatch() throws BubbleDocsException, RepeatedIdentificationException, InvalidValueException,UserNotInSessionException, PermissionException {
+    protected void dispatch() throws BubbleDocsException {
     	User user = this.getSessionFromToken(token).getUser();
-    	user.createUser(username, name, password);    	
+    	
+    	try{
+    		IDRemoteServices remote = new IDRemoteServices();
+    		remote.removeUser(username);
+    	}
+    	
+    	catch(RemoteInvocationException e) {
+    		throw new UnavailableServiceException();
+    		
+    	}
+    	
+    	try{
+    		user.createUser(username, name, email, "randompass");
+    	}
+    	catch(PermissionException e) {
+    		throw new PermissionException("You have no permission to create an user");
+    	}
     }
 }
