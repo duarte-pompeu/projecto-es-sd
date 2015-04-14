4ee4257f757b4980227b8f639bffe82662472800
Marcos Pires <marcos.danix@gmail.com>
2015-04-14 23:02:24 +0100

    Revert 07883aa as asked by Filipe.
    This reverts commit 07883aa531faeef5c702249926c0ee44e36a3eab.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exceptions/InvalidEmailException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exceptions/InvalidEmailException.java
@@ -3,13 +3,5 @@ package pt.tecnico.bubbledocs.exceptions;
 public class InvalidEmailException extends BubbleDocsException {
 
 	private static final long serialVersionUID = -3543607944092143442L;
-	
-	public InvalidEmailException(){
-		super();
-	}
-	
-	public InvalidEmailException(String message){
-		super(message);
-	}
 
 }
