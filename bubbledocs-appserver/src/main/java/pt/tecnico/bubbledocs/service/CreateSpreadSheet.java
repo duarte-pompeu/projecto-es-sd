99fc9c1173904f6df4a3e89998cf503531db1038
Marcos Pires <marcos.danix@gmail.com>
2015-04-16 00:15:59 +0100

    Refactor: Inlined and removed getters and setters in CreateSpreadSheet

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateSpreadSheet.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateSpreadSheet.java
@@ -10,64 +10,31 @@ import pt.tecnico.bubbledocs.exceptions.UserNotInSessionException;
 
 public class CreateSpreadSheet extends BubbleDocsService {
 	 	private CalcSheet result;
-	    private String accessToken;
-	    private String name;
-	    private int rows;
-	    private int columns;
-	    
-//constructor
-    public CreateSpreadSheet(String userToken, String name, int rows,int columns) {
-	    this.setAccessToken(userToken);
-	    this.setName(name);
-	    this.setRows(rows);
-	    this.setColumns(columns);
-    	
-    }
+	 	private String accessToken;
+	 	private String name;
+	 	private int rows;
+	 	private int columns;
 
-    @Override
-    protected void dispatch() throws BubbleDocsException,UserNotInSessionException,InvalidFormatException, InvalidValueException {  
-    	// check if token is in session
-    	User user = getUserFromToken(accessToken);
-    
-    	result = user.createCalcSheet(name, rows, columns);
-    }
+	 	//constructor
+	 	public CreateSpreadSheet(String accessToken, String name, int rows, int columns) {
+	 		this.accessToken = accessToken;
+	 		this.name = name;
+	 		this.rows = rows;
+	 		this.columns = columns;
+	 	}
 
-    
-    //GETTERS AND SETTERS
-	public CalcSheet getResult() {
-		return result;
-	}
 
-	public String getAccessToken() {
-		return accessToken;
-	}
+	 	@Override
+	 	protected void dispatch() throws BubbleDocsException,UserNotInSessionException,InvalidFormatException, InvalidValueException {  
+	 		// check if token is in session
+	 		User user = getUserFromToken(accessToken);
 
-	public void setAccessToken(String accessToken) {
-		this.accessToken = accessToken;
-	}
+	 		result = user.createCalcSheet(name, rows, columns);
+	 	}
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getRows() {
-		return rows;
-	}
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-	public int getColumns() {
-		return columns;
-	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
+	 	public CalcSheet getResult() {
+	 		return result;
+	 	}
 
 }
