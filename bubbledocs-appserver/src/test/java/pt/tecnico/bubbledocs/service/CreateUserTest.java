11d30eae3ef2051249eb9aceef79c860020763a5
Marcos Pires <marcos.danix@gmail.com>
2015-04-16 13:41:12 +0100

    Added mocked verifications
    And changed one thrown exception, DuplicateUsernameException, because
    it's thrown from the remote service. Not
    RepeatedIdentificationException, which should be removed in the future.

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/CreateUserTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/CreateUserTest.java
@@ -2,7 +2,6 @@
 package pt.tecnico.bubbledocs.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import mockit.Expectations;
 import mockit.Mocked;
 import mockit.Verifications;
@@ -10,10 +9,10 @@ import mockit.Verifications;
 import org.junit.Test;
 
 import pt.tecnico.bubbledocs.domain.User;
+import pt.tecnico.bubbledocs.exceptions.DuplicateUsernameException;
 import pt.tecnico.bubbledocs.exceptions.InvalidUsernameException;
 import pt.tecnico.bubbledocs.exceptions.PermissionException;
 import pt.tecnico.bubbledocs.exceptions.RemoteInvocationException;
-import pt.tecnico.bubbledocs.exceptions.RepeatedIdentificationException;
 import pt.tecnico.bubbledocs.exceptions.UnavailableServiceException;
 import pt.tecnico.bubbledocs.exceptions.UserNotInSessionException;
 import pt.tecnico.bubbledocs.exceptions.DuplicateEmailException;
@@ -65,8 +64,13 @@ public class CreateUserTest extends BubbleDocsServiceTest {
         assertEquals(NAME, user.getName());
     }
 
-    @Test(expected =  RepeatedIdentificationException.class)
+    @Test(expected =  DuplicateUsernameException.class)
     public void usernameExists() {
+    	new Expectations() {{
+    		remote.createUser(USERNAME_PRESENT, EMAIL); times = 1;
+			result = new DuplicateUsernameException();
+    	}};
+    	
         CreateUser service = new CreateUser(root_token, USERNAME_PRESENT, EMAIL, NAME);
         service.execute();
     }
@@ -76,6 +80,10 @@ public class CreateUserTest extends BubbleDocsServiceTest {
     public void emptyUsername() {
         CreateUser service = new CreateUser(root_token, "", EMAIL, NAME);
         service.execute();
+        
+        new Verifications() {{
+        	remote.createUser(anyString, anyString); times = 0;
+        }};
     }
 
 	
@@ -84,6 +92,10 @@ public class CreateUserTest extends BubbleDocsServiceTest {
     	String long_name = "Maria Teresa García Ramírez de Arroyo";
     	CreateUser service = new CreateUser(root_token, long_name, EMAIL, "name");
     	service.execute();
+    	
+        new Verifications() {{
+        	remote.createUser(anyString, anyString); times = 0;
+        }};
     }
 	
 	
@@ -91,6 +103,10 @@ public class CreateUserTest extends BubbleDocsServiceTest {
     public void usernameTooShort(){
     	CreateUser service = new CreateUser(root_token, "a", EMAIL, "name");
     	service.execute();
+    	
+        new Verifications() {{
+        	remote.createUser(anyString, anyString); times = 0;
+        }};
     }
 	
     @Test(expected = PermissionException.class)
@@ -98,6 +114,10 @@ public class CreateUserTest extends BubbleDocsServiceTest {
         CreateUser service = new CreateUser(user_token, USERNAME, "queijo",
                 "Sensei");
         service.execute();
+        
+        new Verifications() {{
+        	remote.createUser(anyString, anyString); times = 0;
+        }};
     }
 
     @Test(expected = UserNotInSessionException.class)
@@ -106,33 +126,37 @@ public class CreateUserTest extends BubbleDocsServiceTest {
         CreateUser service = new CreateUser(root_token, USERNAME, "queijo",
                 "Sensei");
         service.execute();
+        
+        new Verifications() {{
+        	remote.createUser(anyString, anyString); times = 0;
+        }};
     }
     
     
     @Test(expected = DuplicateEmailException.class)
     public void DuplicateEmail(){
-    	   new Expectations() {{
-   			remote.createUser(USERNAME,EMAIL); times = 1;
-   			result = new DuplicateEmailException();
-   		}};
-   		
-   		CreateUser service = new CreateUser(root_token, USERNAME, EMAIL, NAME);
-   		service.execute();
+    	new Expectations() {{
+    		remote.createUser(USERNAME,EMAIL); times = 1;
+    		result = new DuplicateEmailException();
+    	}};
+
+    	CreateUser service = new CreateUser(root_token, USERNAME, EMAIL, NAME);
+    	service.execute();
     }
-	
-	@Test(expected = InvalidEmailException.class)
+
+    @Test(expected = InvalidEmailException.class)
     public void InvalidEmail(){
-		
-	    	 new Expectations() {{
-	    			remote.createUser(USERNAME,"im no mail"); times = 1;
-	    			result = new InvalidEmailException();
-	    		}};
-	    		
-	    		CreateUser service = new CreateUser(root_token, USERNAME, "im no mail", NAME);
-	    		service.execute();
-			
+
+    	new Expectations() {{
+    		remote.createUser(USERNAME,"im no mail"); times = 1;
+    		result = new InvalidEmailException();
+    	}};
+
+    	CreateUser service = new CreateUser(root_token, USERNAME, "im no mail", NAME);
+    	service.execute();
+
     }
-    
+
     @Test(expected = UnavailableServiceException.class)
 	public void unavailable() {
 		new Expectations() {{
