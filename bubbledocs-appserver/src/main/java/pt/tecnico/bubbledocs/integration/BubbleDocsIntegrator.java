bf01c09bb6bf244f6731f31e8172f718b69f95c7
Filipe <fmorgad093@gmail.com>
2015-04-24 10:58:03 +0100

    added integration and BBdocsintegrator

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/integration/BubbleDocsIntegrator.java
@@ -0,0 +1,30 @@
+package pt.tecnico.bubbledocs.integration;
+
+import pt.ist.fenixframework.Atomic;
+// add needed import declarations
+import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
+import pt.tecnico.bubbledocs.exceptions.UserNotInSessionException;
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.Session;
+import pt.tecnico.bubbledocs.domain.User;
+
+public abstract class BubbleDocsIntegrator {
+
+    @Atomic
+    public final void execute() throws BubbleDocsException {
+        dispatch();
+    }
+
+    protected abstract void dispatch() throws BubbleDocsException;
+    
+    //Almost every service depends on an active session.
+    protected Session getSessionFromToken(String token) throws UserNotInSessionException {
+    	Session session = BubbleDocs.getInstance().getSessionFromToken(token); 
+    	session.touch();
+    	return session;
+    }
+    
+    protected User getUserFromToken(String token) throws UserNotInSessionException {
+    	return getSessionFromToken(token).getUser();
+    }
+}
