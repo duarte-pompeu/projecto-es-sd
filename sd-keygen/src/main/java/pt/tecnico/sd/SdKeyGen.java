bde4638993b6ced19e48d14ac63e8fa83ff55dc6
Marcos Pires <marcos.danix@gmail.com>
2015-04-30 10:20:19 +0100

    [SD] Create sd-keygen
    Used to create an out of band symmetric key for SD-ID and SD-STORE

---
+++ sd-keygen/src/main/java/pt/tecnico/sd/SdKeyGen.java
@@ -0,0 +1,35 @@
+package pt.tecnico.sd;
+
+import java.io.FileOutputStream;
+import java.security.Key;
+
+import javax.crypto.KeyGenerator;
+
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+
+public class SdKeyGen {
+
+	//Generate a key for 3DES	
+	public static void main(String[] args) throws Exception {
+		System.out.println("Generating 3DES key ..." );
+        
+		KeyGenerator keygen = KeyGenerator.getInstance("DESede");
+        keygen.init(168);
+        Key key = keygen.generateKey();
+        
+        System.out.println("Finished generating 3DES key!");
+        
+        byte[] encoded = key.getEncoded();
+        System.out.printf("Key: \"%s\"%n", printHexBinary(encoded));
+        
+        System.out.println("Writing key to file");
+        FileOutputStream out = new FileOutputStream("secret-key");
+        
+        out.write(encoded);
+        out.close();
+        
+        System.out.println("Done. Shh, keep a secret!");       
+        
+	}
+
+}
