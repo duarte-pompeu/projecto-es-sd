4393f15ce5255446fa772e9e1ae23a6d1dab92a0
duarte-pompeu <pnt12@live.com.pt>
2015-04-30 16:10:01 +0100

    remove compile errors

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/remote/StoreRemoteServices.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/remote/StoreRemoteServices.java
@@ -1,22 +1,32 @@
 package pt.tecnico.bubbledocs.service.remote;
 
 import javax.naming.directory.InvalidAttributeValueException;
+import javax.xml.registry.JAXRException;
 
-import pt.tecnico.bubbledocs.exceptions.CannotStoreDocumentException;
 import pt.tecnico.bubbledocs.exceptions.CannotLoadDocumentException;
+import pt.tecnico.bubbledocs.exceptions.CannotStoreDocumentException;
 import pt.tecnico.bubbledocs.exceptions.RemoteInvocationException;
+import pt.tecnico.bubbledocs.exceptions.UnavailableServiceException;
 import pt.ulisboa.tecnico.sdis.store.cli.StoreClient;
 import pt.ulisboa.tecnico.sdis.store.ws.CapacityExceeded_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.DocDoesNotExist_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.UserDoesNotExist_Exception;
 
 public class StoreRemoteServices {
+	StoreClient client;
 	
+	public StoreRemoteServices(){
+		try {
+			client = new StoreClient();
+		} catch (JAXRException e) {
+			throw new UnavailableServiceException();
+		}
+	}
 	public void storeDocument(String username, String docName, byte[] document)
 			throws CannotStoreDocumentException, RemoteInvocationException {
 		
 		try {
-			StoreClient.storeDoc(username, docName, document);
+			client.storeDoc(username, docName, document);
 		} catch (InvalidAttributeValueException | CapacityExceeded_Exception
 				| DocDoesNotExist_Exception | UserDoesNotExist_Exception e) {
 			
@@ -29,7 +39,7 @@ public class StoreRemoteServices {
 		byte[] result;
 		
 		try {
-			result = StoreClient.loadDoc(username, docName);
+			result = client.loadDoc(username, docName);
 		} catch (InvalidAttributeValueException | DocDoesNotExist_Exception
 				| UserDoesNotExist_Exception e) {
 			
