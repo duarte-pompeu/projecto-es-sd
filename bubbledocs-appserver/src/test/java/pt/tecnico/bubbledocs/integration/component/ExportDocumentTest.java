2bca5958c61571bd1bdfdc377b3d2ee601ae2b55
Marcos Pires <marcos.danix@gmail.com>
2015-05-01 19:56:18 +0100

    Removed now unecessary code in ExportDocumentTest
    Before there was an Integration layer this code was useful, since the
    service had as a result the exported xml. Now the integration doesn't
    return a result, it just sends the exported data to another service.
    This test is supposed to test a service, not exporting XML, which should
    be done in another test case.

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/integration/component/ExportDocumentTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/integration/component/ExportDocumentTest.java
@@ -1,24 +1,9 @@
 package pt.tecnico.bubbledocs.integration.component;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.List;
-
 import pt.tecnico.bubbledocs.service.BubbleDocsServiceTest;
 import pt.tecnico.bubbledocs.service.remote.*;
 import mockit.*;
-
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.filter.Filters;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.xpath.XPathExpression;
-import org.jdom2.xpath.XPathFactory;
 import org.junit.Test;
-
 import pt.tecnico.bubbledocs.domain.Add;
 import pt.tecnico.bubbledocs.domain.CalcSheet;
 import pt.tecnico.bubbledocs.domain.CalcSheetExporter;
@@ -79,7 +64,7 @@ public class ExportDocumentTest extends BubbleDocsServiceTest {
 	
 	
 	@Test
-	public void emptyCalcSheet() throws JDOMException, IOException{		
+	public void emptyCalcSheet() throws Exception {		
 		
 		
 		new Expectations() {{
@@ -88,50 +73,11 @@ public class ExportDocumentTest extends BubbleDocsServiceTest {
 		
 		ExportDocumentIntegrator service = new ExportDocumentIntegrator(U_TOKEN, CS_ID);
 		service.execute();
-		
-		//a local setup
-		SAXBuilder b=new SAXBuilder();
-		Document xmlDoc=b.build(new ByteArrayInputStream(EXPORTED));
-		
-        XPathFactory xFactory = XPathFactory.instance();
-
-        XPathExpression<Element> expr = xFactory.compile("/calcSheet", Filters.element());
-        List<Element> links = expr.evaluate(xmlDoc);
-        Element sheetElement=links.get(0);
-      		
-		String nameReadFromDocument= sheetElement.getAttribute("creator").getValue();
-        int spreadSheetId= sheetElement.getAttribute("id").getIntValue();
-		String spreadSheetDate= sheetElement.getAttribute("date").getValue();
-        String spreadSheetName= sheetElement.getAttribute("name").getValue();
-        int spreadSheetLines = sheetElement.getAttribute("lines").getIntValue();
-        int spreadSheetColumns = sheetElement.getAttribute("columns").getIntValue();
-		
-		//asserting all the attribute values
-		assertEquals("Owner is NOT correct", U_USERNAME, nameReadFromDocument);
-		assertEquals("ID is NOT correct", this.getSpreadSheet(CS_NAME).getId().intValue(), spreadSheetId );
-		assertEquals("Creation date is NOT correct", 
-				this.getSpreadSheet(CS_NAME).getDate().toString(), spreadSheetDate );
-		assertEquals("Name is NOT correct", this.getSpreadSheet(CS_NAME).getName(), spreadSheetName );
-		assertEquals("The number of lines is NOT correct", 
-				this.getSpreadSheet(CS_NAME).getLines().intValue(), spreadSheetLines );
-		assertEquals("The number of columns is NOT correct", 
-				this.getSpreadSheet(CS_NAME).getColumns().intValue(), spreadSheetColumns );
-		
-		//asserting the correct number of cells
-		assertEquals("The number of cells is correct", 0, sheetElement.getChildren().size());
-		
-		
-		//asserting that all the cells have null contents
-		for(Element cellElement: sheetElement.getChildren()){
-			//FIXME: we have 0 cells, so this loop doesn't execute
-			assertEquals("empty cell", 0 ,cellElement.getContentSize());	
-		}
-		
 	}
 	
 	
 	@Test
-	public void calcSheetWithOneCell() throws JDOMException, IOException{
+	public void calcSheetWithOneCell() throws Exception {
 		Cell c=this.getSpreadSheet(CS_NAME).getCell(CELL_ID0);
 		c.setContent(new Literal(7));
 		byte[] exported = new CalcSheetExporter().exportToXmlData(CS_EMPTY);
@@ -142,41 +88,10 @@ public class ExportDocumentTest extends BubbleDocsServiceTest {
 		
 		ExportDocumentIntegrator service = new ExportDocumentIntegrator(U_TOKEN, CS_ID);
 		service.execute();
-		
-		//a local setup
-		SAXBuilder b=new SAXBuilder();
-		Document xmlDoc=b.build(new ByteArrayInputStream(exported));
-		
-        XPathFactory xFactory = XPathFactory.instance();
-
-        XPathExpression<Element> expr = xFactory.compile("/calcSheet", Filters.element());
-        List<Element> links = expr.evaluate(xmlDoc);
-        Element sheetElement=links.get(0);
-        List<Element> cellsList=sheetElement.getChildren();
-        Element literalElement=null;
-        for(Element cellElement: cellsList){
-			if(cellElement.getChild("literal")!=null){
-				literalElement=cellElement.getChild("literal");
-				break;
-			}
-		}
-      
-		
-        //asserting the literal cell exists and has the correct value
-		assertEquals("literal cell is wrong", 7 ,literalElement.getAttribute("val").getIntValue());
-		
-		int counter=0;
-		//asserting that all the other cells have empty contents
-		for(Element cellElement: sheetElement.getChildren()){
-			if(cellElement.getContentSize()!=0)
-				counter++;
-		}
-		assertEquals("non empty cells", counter, 1);
-		
 	}
 	
 	@Test
-	public void calcSheetWithMultipleCells() throws JDOMException, IOException{
+	public void calcSheetWithMultipleCells() throws Exception {
 		Cell c1=this.getSpreadSheet(CS_NAME).getCell(CELL_ID0);
 		c1.setContent(new Literal(7));
 		Cell c2=this.getSpreadSheet(CS_NAME).getCell(CELL_ID1);
@@ -191,67 +106,11 @@ public class ExportDocumentTest extends BubbleDocsServiceTest {
 		
 		ExportDocumentIntegrator service = new ExportDocumentIntegrator(U_TOKEN, CS_ID);
 		service.execute();
-		
-		//a local setup
-		SAXBuilder b=new SAXBuilder();
-		Document xmlDoc=b.build(new ByteArrayInputStream(exported));
-		
-        XPathFactory xFactory = XPathFactory.instance();
-
-        XPathExpression<Element> expr = xFactory.compile("/calcSheet", Filters.element());
-        List<Element> links = expr.evaluate(xmlDoc);
-        Element sheetElement=links.get(0);
-        List<Element> cellsList=sheetElement.getChildren();
-        Element literalElement=null;
-        Element referenceElement=null;
-        Element addElement=null;
-        for(Element cellElement: cellsList){
-        	if(cellElement.getChild("literal")!=null){
-				literalElement=cellElement.getChild("literal");
-				break;
-			}
-        }
-        for(Element cellElement: cellsList){
-        	if(cellElement.getChild("reference")!=null){
-				referenceElement=cellElement.getChild("reference");
-				break;
-			}
-        }
-        for(Element cellElement: cellsList){
-        	if(cellElement.getChild("add")!=null){
-				addElement=cellElement.getChild("add");
-				break;
-			}
-        }
-        
-        Element pointedLiteralElement=referenceElement.getChild("cell").getChild("literal");
-        Element arg1Element=addElement.getChild("referenceArgument");
-        Element pointedLiteralElement2=arg1Element.getChild("cell").getChild("literal");
-        Element arg2Element=addElement.getChild("literalArgument");
-		
-        //asserting the literal cell exists and has the correct value
-		assertEquals("literal cell is wrong", 7 ,literalElement.getAttribute("val").getIntValue());
-		//asserting the reference is pointing to the literal
-		assertEquals("reference DOES NOT point to the literal", 7, 
-				pointedLiteralElement.getAttribute("val").getIntValue());
-		assertEquals("add element DOES NOT HAVE the correct reference and literal", 12, 
-				pointedLiteralElement2.getAttribute("val").getIntValue()+arg2Element.getAttribute("val").getIntValue());
-		
-		
-		int counter=0;
-		
-		for(Element cellElement: sheetElement.getChildren()){
-			if(cellElement.getContentSize()!=0)
-				counter++;
-		}
-		//asserting that all the other cells have empty contents
-		assertEquals("non empty cells", counter, 3);
-		
 	}
 	
 	//Testing the case of trying to export a non existing spread sheet
 	@Test(expected= NotFoundException.class)
-	public void nonExistingCalcSheet() throws JDOMException, IOException{
+	public void nonExistingCalcSheet() throws Exception {
 		new Expectations() {{
 			remote.storeDocument(anyString, anyString, (byte[]) any); times = 0;
 		}};
