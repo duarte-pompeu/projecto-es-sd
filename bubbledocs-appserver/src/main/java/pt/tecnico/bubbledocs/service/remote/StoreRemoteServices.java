ec4f6ce96de4d7fdae6ddd4affb9bca1396bc0ba
duarte-pompeu <pnt12@live.com.pt>
2015-05-05 01:50:39 +0100

    SD - do some frontend work

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/remote/StoreRemoteServices.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/remote/StoreRemoteServices.java
@@ -6,25 +6,28 @@ import javax.xml.registry.JAXRException;
 import pt.tecnico.bubbledocs.exceptions.CannotLoadDocumentException;
 import pt.tecnico.bubbledocs.exceptions.CannotStoreDocumentException;
 import pt.tecnico.bubbledocs.exceptions.RemoteInvocationException;
-import pt.tecnico.bubbledocs.exceptions.UnavailableServiceException;
 import pt.ulisboa.tecnico.sdis.store.cli.ClientFrontEnd;
-import pt.ulisboa.tecnico.sdis.store.cli.StoreClient;
 import pt.ulisboa.tecnico.sdis.store.ws.CapacityExceeded_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.DocDoesNotExist_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.UserDoesNotExist_Exception;
 
 public class StoreRemoteServices {
-	StoreClient client;
+	ClientFrontEnd frontEnd;
 	
 	public StoreRemoteServices(){
-		client = ClientFrontEnd.getClientInstance();
+		try {
+			frontEnd = new ClientFrontEnd();
+		} catch (JAXRException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void storeDocument(String username, String docName, byte[] document)
 			throws CannotStoreDocumentException, RemoteInvocationException {
 		
 		try {
-			client.storeDoc(username, docName, document);
+			frontEnd.storeDoc(username, docName, document);
 		} catch (InvalidAttributeValueException | CapacityExceeded_Exception
 				| DocDoesNotExist_Exception | UserDoesNotExist_Exception e) {
 			
@@ -37,7 +40,7 @@ public class StoreRemoteServices {
 		byte[] result;
 		
 		try {
-			result = client.loadDoc(username, docName);
+			result = frontEnd.loadDoc(username, docName);
 		} catch (InvalidAttributeValueException | DocDoesNotExist_Exception
 				| UserDoesNotExist_Exception e) {
 			
--- sd-store-cli/src/main/java/pt/ulisboa/tecnico/sdis/store/cli/ClientFrontEnd.java
+++ sd-store-cli/src/main/java/pt/ulisboa/tecnico/sdis/store/cli/ClientFrontEnd.java
@@ -11,9 +11,12 @@ import javax.xml.registry.JAXRException;
 import javax.xml.ws.BindingProvider;
 
 import pt.ulisboa.tecnico.sdis.juddi.UDDINaming;
+import pt.ulisboa.tecnico.sdis.store.ws.CapacityExceeded_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.DocAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.store.ws.DocDoesNotExist_Exception;
 import pt.ulisboa.tecnico.sdis.store.ws.SDStore;
 import pt.ulisboa.tecnico.sdis.store.ws.SDStore_Service;
+import pt.ulisboa.tecnico.sdis.store.ws.UserDoesNotExist_Exception;
 
 public class ClientFrontEnd {
 	private static final String DEFAULT_UDDI_NAME = "SD-STORE";
@@ -23,6 +26,7 @@ public class ClientFrontEnd {
 	
 	private static Collection<StoreClient> _clients = new ArrayList<StoreClient>();
 	private String _uddiName = "SD-STORE";
+	int min4quorum;
 	
 	
 	public ClientFrontEnd(String uddiName) throws JAXRException{
@@ -34,25 +38,24 @@ public class ClientFrontEnd {
 		this(DEFAULT_UDDI_NAME);
 	}
 	
-	public void createDoc(String userID, String docID) throws InvalidAttributeValueException, DocAlreadyExists_Exception{
-		for(StoreClient client : _clients){
-			client.createDoc(userID, docID);
-		}
-	}
-	
 	
 	public void init() throws JAXRException{
 		if(VERBOSE){
 			System.out.println("ENDPOINT ADDRESSES ... START ...");
 		}
+	
+		int n_servers = 0;
 		
 		for(String endpointAddress: listEndpoints()){
 			_clients.add(initClient(endpointAddress));
+			n_servers++;
 			
 			if(VERBOSE){
 				System.out.println(endpointAddress);
 			}
 		}
+		
+		this.min4quorum = (n_servers/2);
 		
 		if(VERBOSE){
 			System.out.println("ENDPOINT ADDRESSES ... END ...");
@@ -82,5 +85,58 @@ public class ClientFrontEnd {
 		}
 		
 		return endpoints;
+	}
+	
+	
+	private int getMinForQuorumConsensus() {
+		// TODO Auto-generated method stub
+		return min4quorum;
+	}
+
+	public void createDoc(String userID, String docID) throws InvalidAttributeValueException{
+		ArrayList <Exception> exceptions = new ArrayList <Exception>();
+		ArrayList <InvalidAttributeValueException> IAVEs = new ArrayList <InvalidAttributeValueException>();
+		ArrayList <DocAlreadyExists_Exception> DAEEs = new ArrayList <DocAlreadyExists_Exception>();
+		
+		for(StoreClient client : _clients){
+			try {
+				client.createDoc(userID, docID);
+			} catch (InvalidAttributeValueException e) {
+				exceptions.add(e);
+				IAVEs.add(e);
+			} catch (DocAlreadyExists_Exception e) {
+				exceptions.add(e);
+				DAEEs.add(e);
+			}
+		}
+		
+		if(exceptions.size() < getMinForQuorumConsensus()){
+			// all should be good
+			return;
+		}
+		
+		if(IAVEs.size() >= getMinForQuorumConsensus()){
+			throw new InvalidAttributeValueException();
+		}
+		
+		if(IAVEs.size() >= getMinForQuorumConsensus()){
+			throw IAVEs.get(0);
+		}
+	}
+	
+	public void storeDoc(String userID, String docID, byte[] content) throws InvalidAttributeValueException, CapacityExceeded_Exception, DocDoesNotExist_Exception, UserDoesNotExist_Exception {
+		for(StoreClient client : _clients){
+			client.storeDoc(userID, docID, content);
+		}
+	}
+	
+
+	public byte[] loadDoc(String userID, String docID) throws InvalidAttributeValueException, DocDoesNotExist_Exception, UserDoesNotExist_Exception {
+		for(StoreClient client : _clients){
+			client.loadDoc(userID, docID);
+		}
+		
+		
+		return null;
 	}
 }
