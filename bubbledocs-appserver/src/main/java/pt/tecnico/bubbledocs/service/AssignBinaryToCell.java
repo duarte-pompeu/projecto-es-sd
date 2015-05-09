08ec311166e97e8b0b504237c4c3a4e312fcc9d0
Marcos Pires <marcos.danix@gmail.com>
2015-05-10 00:24:53 +0100

    Idented the code

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignBinaryToCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignBinaryToCell.java
@@ -25,10 +25,10 @@ public class AssignBinaryToCell extends BubbleDocsService {
 	private String cellId;
 	private String FunctionExp;
 	private String Result = "";
-	
-	
+
+
 	public AssignBinaryToCell(String CellId, String Function, int CsId, String UserToken) {
-	
+
 		this.accessToken = UserToken;
 		this.docId = CsId;
 		this.cellId = CellId;
@@ -38,9 +38,9 @@ public class AssignBinaryToCell extends BubbleDocsService {
 
 	@Override
 	public void dispatch() throws BubbleDocsException 	{
-		
-		
-		
+
+
+
 		// token in session
 		BubbleDocs bd = BubbleDocs.getInstance();
 		User user;
@@ -50,107 +50,107 @@ public class AssignBinaryToCell extends BubbleDocsService {
 		catch(UserNotInSessionException e){
 			throw e;
 		}
-		
+
 		CalcSheet c1 = bd.getCalcSheetById(docId);
 		Cell cell = c1.getCell(cellId);
-		
-		
+
+
 		if (cell == null) throw new NotFoundException("Cell out of bounds in " + cellId);
-		
-		
+
+
 		CalcSheet cs = null;
-    	for(CalcSheet tempCs: bd.getCalcSheetSet()){
-    		if(tempCs.getId() == docId){
-    			cs = tempCs;
-    		}
-    	}
-    	
-    	if(cs == null){
-    		throw new NotFoundException("can't find calcsheet with ID " + docId + ".");
-    	}
-    	
-    	
-    	//TODO: check if user has write access
-    	if(!user.canWrite(cs)){
-    		throw new PermissionException();
-    	}
-		
-		
-		
-		
-		
+		for(CalcSheet tempCs: bd.getCalcSheetSet()){
+			if(tempCs.getId() == docId){
+				cs = tempCs;
+			}
+		}
+
+		if(cs == null){
+			throw new NotFoundException("can't find calcsheet with ID " + docId + ".");
+		}
+
+
+		//TODO: check if user has write access
+		if(!user.canWrite(cs)){
+			throw new PermissionException();
+		}
+
+
+
+
+
 		//Lets check the expression
 
 		Pattern p = Pattern.compile("=(ADD|SUB|MUL|DIV)\\(([1-9]+[0-9]*,[1-9]+[0-9]*|[1-9]+[0-9]*);([1-9]+[0-9]*,[1-9]+[0-9]*|[1-9]+[0-9]*)\\)");
-       Matcher m = p.matcher(FunctionExp);
-         
-		 if ( !m.matches() ) {
-					
-					throw new InvalidFormatException();
-					
-		 }		
-		
-         //System.out.println("we have a match!!"); 
-		
+		Matcher m = p.matcher(FunctionExp);
+
+		if ( !m.matches() ) {
+
+			throw new InvalidFormatException();
+
+		}		
+
+		//System.out.println("we have a match!!"); 
+
 		BinaryFunction Func2apply = bd.parseNameToBin(FunctionExp.substring(1, 4)) ;
-		
-		
+
+
 		Content argument4func1, argument4func2 ; 
-		
+
 		String[] parsed = FunctionExp.split(";");
-		
+
 		int openbracket_index = parsed[0].indexOf("(");
 		int ending_index = parsed[0].length() ; 
 		int comma_index = parsed[0].indexOf(","); 
 		//int closebracket_index = parsed[0].indexOf(")");
-		
+
 		if (comma_index != -1) { 
-		    
-		    //System.out.println("this is a ref pointing to " + parsed[0].substring(openbracket_index + 1, comma_index) +"," + parsed[0].substring(comma_index + 1, ending_index) ) ; 
+
+			//System.out.println("this is a ref pointing to " + parsed[0].substring(openbracket_index + 1, comma_index) +"," + parsed[0].substring(comma_index + 1, ending_index) ) ; 
 			argument4func1 = new Reference ( new Cell ( Integer.valueOf( parsed[0].substring(openbracket_index + 1, comma_index)) ,  Integer.valueOf(parsed[0].substring(comma_index + 1, ending_index) )    )      ) ; 
-			
-			
+
+
 		} else {
-					//System.out.println("this is a literal : " + parsed[0].substring(openbracket_index + 1, ending_index ));
-					argument4func1 =  new Literal( Integer.valueOf( parsed[0].substring(openbracket_index + 1, ending_index ) ) );
-					
-					}
-		
-		
-		
+			//System.out.println("this is a literal : " + parsed[0].substring(openbracket_index + 1, ending_index ));
+			argument4func1 =  new Literal( Integer.valueOf( parsed[0].substring(openbracket_index + 1, ending_index ) ) );
+
+		}
+
+
+
 		int closebracket_index = parsed[1].indexOf(")");
 		comma_index = parsed[1].indexOf(","); 
 		//int closebracket_index = parsed[0].indexOf(")");
 		if (comma_index != -1) { 
-		    
-		    //System.out.println("this is a ref pointing to " + parsed[1].substring(0, comma_index) +"," + parsed[1].substring(comma_index + 1, closebracket_index ) ) ; 
+
+			//System.out.println("this is a ref pointing to " + parsed[1].substring(0, comma_index) +"," + parsed[1].substring(comma_index + 1, closebracket_index ) ) ; 
 			argument4func2 = new Reference ( new Cell( Integer.valueOf(parsed[1].substring(0, comma_index)) ,  Integer.valueOf(parsed[1].substring(comma_index + 1, closebracket_index )))) ; 
-			
+
 		} else {
-					//System.out.println("this is a literal : " + parsed[1].substring(0, closebracket_index ));
-					argument4func2 = new Literal (Integer.valueOf( parsed[1].substring(0, closebracket_index ) ));
-					}
-		
-		
-		
-		
-		
-	
+			//System.out.println("this is a literal : " + parsed[1].substring(0, closebracket_index ));
+			argument4func2 = new Literal (Integer.valueOf( parsed[1].substring(0, closebracket_index ) ));
+		}
+
+
+
+
+
+
 		/*
-		
+
 		O metodo init de BinaryFunction recebe FunctionArgument, e argument4func1 e argument4func2 sao Content com o literal/referencia. Convert? 
-		
-		
-	
+
+
+
 		Func2apply.init( argument4func1, argument4func2);
-	
-	
-	
+
+
+
 		cs.setContent(user, Func2apply, cellId);
-		
-		
-		
-		*/
+
+
+
+		 */
 	}
 
 	public final String getResult() {
