2e1bb51a29a564f2cb655275527c3d38ccd6377a
Marcos Pires <marcos.danix@gmail.com>
2015-05-12 14:08:39 +0100

    Removed useless code and imports in AssignBinarytoCell

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignBinaryToCell.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/AssignBinaryToCell.java
@@ -1,33 +1,12 @@
 package pt.tecnico.bubbledocs.service;
 //imports
-import pt.tecnico.bubbledocs.domain.Add;
 import pt.tecnico.bubbledocs.domain.BubbleDocs;
 import pt.tecnico.bubbledocs.domain.CalcSheet;
-import pt.tecnico.bubbledocs.domain.Cell;
-import pt.tecnico.bubbledocs.domain.Div;
-import pt.tecnico.bubbledocs.domain.Literal;
-import pt.tecnico.bubbledocs.domain.LiteralArgument;
-import pt.tecnico.bubbledocs.domain.Mul;
-import pt.tecnico.bubbledocs.domain.Reference;
-import pt.tecnico.bubbledocs.domain.ReferenceArgument;
-import pt.tecnico.bubbledocs.domain.Sub;
-import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.domain.BinaryFunction;
-import pt.tecnico.bubbledocs.domain.FunctionArgument;
-import pt.tecnico.bubbledocs.domain.Content;
 import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
-import pt.tecnico.bubbledocs.exceptions.NotFoundException;
 import pt.tecnico.bubbledocs.exceptions.NullContentException;
-import pt.tecnico.bubbledocs.exceptions.UserNotInSessionException;
-import pt.tecnico.bubbledocs.exceptions.InvalidFormatException;
-import pt.tecnico.bubbledocs.exceptions.PermissionException;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class AssignBinaryToCell extends SessionService {
-	private Content result;
-	private String accessToken;
 	private int docId;
 	private String cellId;
 	private String FunctionExp;
@@ -40,16 +19,7 @@ public class AssignBinaryToCell extends SessionService {
 		this.cellId = CellId;
 		this.FunctionExp = Function;
 
-	}
-
-	private static final String OPERATION = "(ADD|SUB|MUL|DIV)";
-	private static final String POSITIVE_NUM = "([1-9][0-9]*)";
-	private static final String LITERAL = "(0|([+-]?[1-9][0-9]*))";
-	private static final String REFERENCE = "(" + POSITIVE_NUM + ";" + POSITIVE_NUM + ")";
-	private static final String ARGUMENT = "(" + LITERAL + "|" + REFERENCE + ")";
-	private static final String EXPRESSION = "=" + OPERATION + "\\(" + ARGUMENT + "," + ARGUMENT + "\\)";
-	
-	
+	}	
 	
 	@Override
 	public void dispatchAfterSuperService() throws BubbleDocsException 	{
